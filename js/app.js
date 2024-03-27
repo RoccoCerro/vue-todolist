eaec93a74604512ed6714ec9ce046af7be0d0da1
RoccoCerro <rcerro02@gmail.com>
2024-03-27 14:49:40 +0100

    Add: milestone 1, stampati tanti li quanti gli oggetti presenti in todos e applicata la classe per sbarrare un item

--- js/app.js
+++ js/app.js
@@ -3,7 +3,27 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      message: 'Hello Vue!'
+      todos: [
+        {
+          text: 'Comprare il pane',
+          done: false
+        },
+        {
+          text: 'Comprare il sapone',
+          done: true
+        },
+        {
+          text: 'Comprare il prosciutto',
+          done: false
+        },
+        {
+          text: 'Comprare l\'arrosto',
+          done: false
+        }
+      ]
     }
+  },
+  methods: {
+
   }
 }).mount('#app')
